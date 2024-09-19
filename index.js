d01f8569ec4f5239dcfd9afbe83efc0f2d6c3414
Ricardo Augusto <166884977+RicardoAugust-0@users.noreply.github.com>
2024-09-19 20:35:31 -0300

    Update index.js

--- index.js
+++ index.js
@@ -9,7 +9,8 @@ let products = [];
 
 // Requisição para adicionar produtos
 app.post("/products", (req, res) => {
-  const { nome, descricao, preco, quantidade, peso, disponibilidade } = req.body;
+  const { nome, descricao, preco, quantidade, peso, disponibilidade } =
+    req.body;
   const newProduct = {
     id: products.length + 1,
     nome,
@@ -17,34 +18,73 @@ app.post("/products", (req, res) => {
     preco,
     quantidade,
     peso,
-    disponibilidade
+    disponibilidade,
   };
-  const produtoExistente = products.find(product => product.nome === nome);
+  const produtoExistente = products.find((product) => product.nome === nome);
   if (produtoExistente) {
-    res.status(400).json({ message: "Produto não cadastrado."});
+    res.status(400).json({ message: "Produto já cadastrado." });
   } else {
     products.push(newProduct);
-    res.status(201).json({ message: "Produto cadastrado no sistema!", product: newProduct});
+    res
+      .status(201)
+      .json({ message: "Produto cadastrado no sistema!", product: newProduct });
   }
 });
 
 //Requisitando algum produto especifíco pelo ID
 app.get("/products/:id", (req, res) => {
   const productId = req.params.id;
-  const product = products.find((product) => product.id === productId);
+  const product = products.find(
+    (product) => product.id === parseInt(productId)
+  );
 
   if (!product) {
-    res.status(404).json({ message: "Produto não encontrado"});
+    res.status(404).json({ message: "Produto não encontrado" });
   } else {
-    res.status(200).json({ message: "Produto encontrado:", product: product});
+    res.status(200).json({ message: "Produto encontrado:", product: product });
   }
 });
 
+// Requisitar todos os produtos
 app.get("/products", (req, res) => {
-    const productsList = products;
-    res.status(200).json({ message: "Lista de produtos:", products: productsList});
+  const productsList = products;
+  res
+    .status(200)
+    .json({ message: "Lista de produtos:", products: productsList });
 });
 
+// Atualizar algum produto
+app.put("/products/:id", (req, res) => {
+  const { nome, descricao, preco, quantidade, peso, disponibilidade } =
+    req.body;
+  const productId = parseInt(req.params.id);
+  const product = products.find((product) => product.id === productId);
+  
+  if (!product) {
+    res.status(404).json({ message: "Produto não encontrado" });
+  } else {
+    product.nome = nome;
+    product.descricao = descricao;
+    product.preco = preco;
+    product.quantidade = quantidade;
+    product.peso = peso;
+    product.disponibilidade = disponibilidade;
+    res.status(200).json({ message: "Produto atualizado!", product: product });
+  }
+});
+
+// Excluir algum produto
+app.delete("/products/:id", (req, res) => {
+  const productId = parseInt(req.params.id);
+  const product = products.find((product) => product.id === productId);
+  if (!product) {
+    res.status(404).json({ message: "Produto não encontrado" });
+  } else {
+    const index = products.indexOf(product);
+    products.splice(index, 1);
+    res.status(200).json({ message: "Produto excluído!" });
+  }
+});
 
 let port = 7777;
 app.listen(port, () => {
